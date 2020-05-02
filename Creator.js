cb31ed95ae673f4c0a79ce4a8eec6397d9d8b6dd
AleksejBas <aleksej2203@yandex.ru>
2020-05-02 22:48:16 +0700

    Update Creator.js

--- Creator.js
+++ Creator.js
@@ -1,2 +1,7 @@
 
 console.log('Hello, World!!!');
+<script>
+    let link = document.querySelector('link[rel="import"]');
+    let content = link.import.querySelector('#message');
+    document.body.appendChild(content.cloneNode(true));
+</script> 
